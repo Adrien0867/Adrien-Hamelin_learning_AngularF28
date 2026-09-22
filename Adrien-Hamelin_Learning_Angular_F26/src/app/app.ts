1b9c5c9c74e483c11fd6442304e8f430711dff03
Adrien0867 <AH284@myscc.ca>
2026-09-22 14:39:12 -0400

    Created starter Angular file and completed Assignment 1

--- Adrien-Hamelin_Learning_Angular_F26/src/app/app.ts
+++ Adrien-Hamelin_Learning_Angular_F26/src/app/app.ts
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Adrien-Hamelin_Learning_Angular_F26');
+
+  // Step 8: Two new typed variables
+  studentName: string = 'Adrien Hamelin';
+  courseTitle: string = 'MAD 307 Javascript Frameworks';
 }
